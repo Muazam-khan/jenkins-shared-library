4866eccbed44f99e1addc79ba2730b5a7bcceb8c
Muazam-khan <kmuazam6@gmail.com>
2023-12-19 14:48:47 -0500

    Auto Push Jenkins-shared-library

---
+++ vars/maven.groovy
@@ -0,0 +1,25 @@
+      def lintChecks(){          
+            sh "echo ***** Starting Style Checks for ${COMPONENT}  *****"
+            sh "/home/centos/node_modules/jslint/bin/jslint.js server.js || true" // this cmd does style check for server.js
+            sh "echo ***** Style Checks are Completed for ${COMPONENT} *****"
+      }     
+
+      def call()  { //when u call file nodejs, this function will be called by default, call is default func
+            pipeline {
+              agent any     
+                stages{
+                    stage('Lint Checks'){
+                    steps {
+                       script {
+                         lintChecks() //call func is calling another func lintchecks
+                        }           
+                    }          
+                }
+                stage('Static Code Analysis'){
+                    steps {
+                       sh "echo ***** Starting Static Code Analysis *****"          
+                   }          
+               }
+           }
+       }  
+   }
