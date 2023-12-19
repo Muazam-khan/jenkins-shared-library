3b21f99c10e491bad783f80bec7a638f46d4b450
Muazam-khan <kmuazam6@gmail.com>
2023-12-19 15:00:03 -0500

    Auto Push Jenkins-shared-library

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -5,13 +5,13 @@
       }     
 
       def call()  { //when u call file nodejs, this function will be called by default, call is default func
-            pipeline {
+          pipeline {
               agent any     
                 stages{
                     stage('Lint Checks'){
                     steps {
                        script {
-                         lintChecks() //call func is calling another func lintchecks
+                          lintChecks() //call func is calling another func lintchecks
                         }           
                     }          
                 }
