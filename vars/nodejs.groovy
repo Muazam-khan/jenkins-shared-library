b4c6fbad602c19ee490e884c3bfcbace98c95707
Muazam-khan <kmuazam6@gmail.com>
2023-12-19 13:35:22 -0500

    Auto Push Jenkins-shared-library

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,5 +1,5 @@
-      def lintChecks(){          
-            sh "echo ***** Starting Style Checks *****"
+      def lintChecks('component'){          
+            sh "echo ***** Starting Style Checks for ${component} *****"
             sh "ls -ltr server.js"
             sh "/home/centos/node_modules/jslint/bin/jslint.js server.js || true" // this cmd does style check for server.js
             sh "echo ***** Completed Style Checks *****"
@@ -12,7 +12,7 @@
             stage('Lint Checks'){
                 steps {
                     script {
-                        nodejs.lintChecks()
+                        nodejs.lintChecks("component")
                     }           
                 }          
             }
