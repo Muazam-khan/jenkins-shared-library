e0637fa1da2b12f77fe5c3baf3dbd52c212c5450
Muazam-khan <kmuazam6@gmail.com>
2023-12-19 12:10:08 -0500

    Auto Push Jenkins-shared-library

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,7 +1,7 @@
 // info is the default function that will be called when you call the variable
 
-def info() {
-    echo "INFO : demo on shared library"
+def info(message) {
+    echo "INFO : demo on shared library with ${message}"
 }
 
 info()
