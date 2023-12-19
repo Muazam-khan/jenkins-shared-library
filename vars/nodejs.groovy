b1e82b6a6f737ea17cf43fbc0e5a35b6ef55adef
Muazam-khan <kmuazam6@gmail.com>
2023-12-19 14:06:17 -0500

    Auto Push Jenkins-shared-library

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -2,7 +2,7 @@
             sh "echo ***** Starting Style Checks for ${COMPONENT}  *****"
             sh "ls -ltr server.js"
             sh "/home/centos/node_modules/jslint/bin/jslint.js server.js || true" // this cmd does style check for server.js
-            sh "echo ***** Completed Style Checks *****"
+            sh "echo ***** Style Checks are Completed for ${COMPONENT} *****"
       }     
 
       def call()  { //when u call file nodejs, this function will be called by default
