c8d9a27df5a673f6700fa088d200ce9bf4bfe5cf
Muazam-khan <kmuazam6@gmail.com>
2023-12-19 13:33:20 -0500

    Auto Push Jenkins-shared-library

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -3,4 +3,24 @@
             sh "ls -ltr server.js"
             sh "/home/centos/node_modules/jslint/bin/jslint.js server.js || true" // this cmd does style check for server.js
             sh "echo ***** Completed Style Checks *****"
-      }       
+      }     
+
+      def call()  { //when u call file nodejs, this function will be called by default
+            pipeline {
+            agent any     
+            stages{
+            stage('Lint Checks'){
+                steps {
+                    script {
+                        nodejs.lintChecks()
+                    }           
+                }          
+            }
+                stage('Static Code Analysis'){
+                steps {
+                    sh "echo ***** Starting Static Code Analysis *****"          
+                 }          
+             }
+         }
+     }  
+}
