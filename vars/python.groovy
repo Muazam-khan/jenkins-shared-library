7c635e9c003f2e3f201f09ac7721a2fa644582bf
Muazam-khan <kmuazam6@gmail.com>
2023-12-20 13:25:50 -0500

    Auto Push Jenkins-shared-library

--- vars/python.groovy
+++ vars/python.groovy
@@ -18,7 +18,23 @@
                 }
                 stage('Static Code Analysis'){
                     steps {
-                       sh "echo ***** Starting Static Code Analysis *****"          
+                        script {
+                         sonarChecks() //call func is calling another func lintchecks
+                        }            
+                   }          
+               }
+                 stage('Get the Sonar result'){
+                    steps {
+                        sh "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > gates.sh"
+                        sh "./gates.sh admin password ${SONAR_URL} ${COMPONENT}"
+                    }
+                 }
+               stage('Unit Testing'){
+                    steps {
+                        script {
+                         sh "echo Testing in process" //call func is calling another func lintchecks
+                         sh "echo Testing is completed"
+                        }            
                    }          
                }
            }
