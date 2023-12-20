ba55d4068a44ac5fa35cb10ab2b955fbc0ca439a
Muazam-khan <kmuazam6@gmail.com>
2023-12-20 14:04:56 -0500

    Auto Push Jenkins-shared-library

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -29,7 +29,7 @@
                  stage('Get the Sonar result'){
                     steps {
                         sh "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > gates.sh"
-                        sh "./gates.sh admin password ${SONAR_URL} ${COMPONENT}"
+                        sh "bash gates.sh admin password ${SONAR_URL} ${COMPONENT}"
                     }
                  }
                stage('Unit Testing'){
