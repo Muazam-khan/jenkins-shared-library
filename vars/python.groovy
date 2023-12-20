8f16ebf0a455056d15a763f0f1576417d5b55d19
Muazam-khan <kmuazam6@gmail.com>
2023-12-20 14:04:17 -0500

    Auto Push Jenkins-shared-library

--- vars/python.groovy
+++ vars/python.groovy
@@ -7,7 +7,7 @@
           def sonarChecks(){
                 sh "sonar-scanner -Dsonar.host.url=http://172.31.47.174:9000 -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=admin -Dsonar.password=password"
              } 
-             
+
       def call()  { //when u call file nodejs, this function will be called by default, call is default func
           pipeline {
               agent any     
@@ -29,7 +29,7 @@
                  stage('Get the Sonar result'){
                     steps {
                         sh "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > gates.sh"
-                        sh "./gates.sh admin password ${SONAR_URL} ${COMPONENT}"
+                        sh "bash gates.sh admin password ${SONAR_URL} ${COMPONENT}"
                     }
                  }
                stage('Unit Testing'){
