b061e063733c58029558a77d3e4948b1ccc8ca76
Muazam-khan <kmuazam6@gmail.com>
2023-12-20 14:52:13 -0500

    Auto Push Jenkins-shared-library

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,3 +1,3 @@
  def sonarChecks(){
-        sh "sonar-scanner -Dsonar.host.url=http://172.31.47.174:9000 -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=admin -Dsonar.password=password"
+        sh "sonar-scanner -Dsonar.host.url=http://172.31.47.174:9000 ${ARGS} -Dsonar.projectKey=${COMPONENT} -Dsonar.login=admin -Dsonar.password=password"
       }
