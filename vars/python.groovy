a9c6868cac8e5b9c08129339c73b98c08d2eceac
Muazam-khan <kmuazam6@gmail.com>
2023-12-20 13:28:16 -0500

    Auto Push Jenkins-shared-library

--- vars/python.groovy
+++ vars/python.groovy
@@ -3,8 +3,11 @@
              //   sh "pip install pylint" // this cmd does style check for server.js
           // sh "pylint *.py || true" this cmd does style check for server.js
             sh "echo ***** Style Checks are Completed for ${COMPONENT} *****"
-      }     
-
+      }   
+          def sonarChecks(){
+                sh "sonar-scanner -Dsonar.host.url=http://172.31.47.174:9000 -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=admin -Dsonar.password=password"
+             } 
+             
       def call()  { //when u call file nodejs, this function will be called by default, call is default func
           pipeline {
               agent any     
